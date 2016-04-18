51b6b852e61725678c46e4a9520d528c029c76e6
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-04-18 18:17:49 +0900

    fix two bugs in the rc.subr script
    influxd_flags, which usually means command line options to `command`
    but when you override `command`, daemon(8) in this case, in the script,
    the variable is passed to the command you specified. thus, setting
    influxd_flags in rc.conf(5) produces "invalid argument". the correct
    variable is influxdb_flags.
    
    it called load_rc_config *after* setting the defaults and expanding the
    variables, which effectively *disables* overriding mechanism. thus,
    influxdb_flags was never be applied to the command arguments.

--- spec/serverspec/default_spec.rb
+++ spec/serverspec/default_spec.rb
@@ -49,3 +49,29 @@ describe file(db_dir) do
   it { should be_owned_by user }
   it { should be_grouped_into group }
 end
+
+case os[:family]
+when 'freebsd'
+  describe file('/etc/rc.conf.d/influxd') do
+    it { should be_file }
+    its(:content) { should match Regexp.escape('influxd_user="influxd"') }
+    its(:content) { should match Regexp.escape('influxd_group="influxd"') }
+    its(:content) { should match Regexp.escape('influxd_conf="/usr/local/etc/influxd.conf"') }
+    its(:content) { should match Regexp.escape('influxdb_flags="-join foo.example.com:8888"') }
+  end
+end
+
+if 1 == 0
+# XXX serverspec uses 'ps -C procname -o user=' but it is a Linux specific flag
+# TODO create a patch
+# /usr/local/bin/influxd -join foo.example.com:8888 -config=/usr/local/etc/influxd.conf
+describe process("influxd") do
+  its(:user) { should eq "influxd" }
+  its(:args) { should match Regexp.escape('-join foo.example.com:8888 -config=/usr/local/etc/influxd.conf') }
+end
+end
+# the work around of above issue
+describe command('ps axwwu') do
+  its(:stdout) { should match /^influxd .*/ }
+  its(:stdout) { should match Regexp.escape('influxd -join foo.example.com:8888 -config=/usr/local/etc/influxd.conf') }
+end
