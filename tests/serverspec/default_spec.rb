66195274260d80fe79817641c2617452df4af3af
mitsururike <mitsururike2@gmail.com>
2017-12-25 15:32:27 +0900

    [bugfix] fix third party repo, bump rubocop and QA (#1)
    * [bugfix] fix thirdparty repo, bump rubocop and QA
    
    * fix the failed previous commit
    
    * [bugfix] fix provisioner and directory hierarchy
    
    * [bugfix] fix travisci
    
    * QA
    
    * [bugfix] fix obsoleted config and flags; fix specs as well
    
    - use default config
    - `--join` flag is not available
    - web admin interface is no longer available

---
+++ tests/serverspec/default_spec.rb
@@ -0,0 +1,77 @@
+require "spec_helper"
+require "serverspec"
+
+package = "influxdb"
+service = "influxd"
+config  = "/etc/influxdb/influxd.conf"
+user    = "influxd"
+group   = "influxd"
+ports   = [8086, 8088]
+log_dir = "/var/log/influxdb"
+db_dir  = "/var/lib/influxdb"
+
+case os[:family]
+when "freebsd"
+  config = "/usr/local/etc/influxd.conf"
+  db_dir = "/var/db/influxdb"
+end
+
+describe package(package) do
+  it { should be_installed }
+end
+
+describe service(service) do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+ports.each do |p|
+  describe port(p) do
+    it { should be_listening }
+  end
+end
+
+describe file(config) do
+  it { should be_file }
+  its(:content) { should match Regexp.escape('dir = "/var/db/influxdb/meta"') }
+end
+
+describe file(log_dir) do
+  it { should exist }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+describe file(db_dir) do
+  it { should exist }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+case os[:family]
+when "freebsd"
+  describe file("/etc/rc.conf.d/influxd") do
+    it { should be_file }
+    its(:content) { should match Regexp.escape('influxd_user="influxd"') }
+    its(:content) { should match Regexp.escape('influxd_group="influxd"') }
+    its(:content) { should match Regexp.escape('influxd_conf="/usr/local/etc/influxd.conf"') }
+    its(:content) { should match Regexp.escape('influxdb_flags=""') }
+  end
+end
+
+if 1.zero?
+  # XXX serverspec uses 'ps -C procname -o user=' but it is a Linux specific flag
+  # TODO create a patch
+  # /usr/local/bin/influxd -join foo.example.com:8888 -config=/usr/local/etc/influxd.conf
+  describe process("influxd") do
+    its(:user) { should eq "influxd" }
+    its(:args) { should match Regexp.escape("-config=/usr/local/etc/influxd.conf") }
+  end
+end
+# the work around of above issue
+describe command("ps axwwu") do
+  its(:stdout) { should match(/^influxd .*/) }
+  its(:stdout) { should match Regexp.escape("influxd -config=/usr/local/etc/influxd.conf") }
+end
